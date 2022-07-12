35d7553824e1ebef4457546ee87df514f1798bb5
MoniqueAraujo <moniquemedeiiros@outlook.com>
2022-07-12 00:22:14 -0300

    feat:adicionado animação json, em apresentação

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,12 +1,31 @@
 import './styles.css';
+import Lottie from 'react-lottie';
+import woman from '../../animation/woman.json'
 
 function Welcome() {
+    const defaultOptions = {
+        loop: false,
+        autoplay: true,
+        animationData: woman,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+
     return (
         <div className='container-apresentation'>
             <div className='container-name'>
                 <span className='apresentation'>Olá, meu nome é</span>
                 <div><span className='name-pink' style={{ color: '#F1889B' }}>Mon</span><span className='name-white' style={{ color: '#ffffff' }}>ique</span> <span className='name-pink' style={{ color: '#F1889B' }}>Ara</span><span className='name-white' style={{ color: '#ffffff' }}>ujo</span></div>
             </div>
+            <div>
+                <Lottie options={defaultOptions}
+                    width={423}
+                    height={276}
+
+                />
+            </div>
+
         </div>
 
     )
